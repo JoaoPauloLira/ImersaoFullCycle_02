ae84fe0c6e9eccc262c68651899616f6e51b6d34
João Paulo Lira da Silva <joao.p.lira@gmail.com>
2022-10-21 10:07:11 -0300

    Merge
    Merge

--- nest-api/src/app.service.ts
+++ nest-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! João Paulo 11:20';
+    return 'Hello World! João Paulo 09:57';
   }
 }
